6919e87f4d4be57e15a9bf017663badcc0417b08
Edvard Sveshnikov <esbarros@proton.me>
2025-05-27 21:16:03 -0300

    Update agree.c

--- agree.c
+++ agree.c
@@ -8,7 +8,15 @@ int main(void)
     {
         printf("Agreed.\n");
     }
+    else if (c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
     else if (c == 'n')
+    {
+        printf("Not agreed.\n");
+    }
+    else if (c == 'N')
     {
         printf("Not agreed.\n");
     }
