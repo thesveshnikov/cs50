356cdd56071031b00283ed8c20d48efa0adc14b4
Edvard Sveshnikov <esbarros@proton.me>
2025-05-27 21:34:31 -0300

    Update cat.c

--- cat.c
+++ cat.c
@@ -1,10 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void meow(void)
+{
+    printf("meow\n");
+}
+
 int main(void)
 {
-    while (true)
+    for (int i = 0; i < 3; i++)
     {
-        print("meow\n");
+        meow();
     }
+    
 }
