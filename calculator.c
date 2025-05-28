fdc46b81ef7cf4ec67d73a994902f32c65659c53
Sveshnikov <esbarros@proton.me>
2025-05-28 20:13:21 -0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,18 +3,8 @@
 
 int main(void)
 {
-    long dollars = 1;
-    while (true)
-    {
-        char c = get_char("Here's $%li. Double it and give it to the next person? ", dollars);
-        if (c == 'y')
-        {
-            dollars *= 2;
-        }
-        else
-        {
-            break;
-        }
-    }
-    printf("Here's $%li.\n", dollars);
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    
+    printf("%i\n", x / y);
 }
