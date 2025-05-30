6f451f65d36521ec4587ac60eb974b719b60565c
Sveshnikov <esbarros@proton.me>
2025-05-29 23:53:03 -0300

    Create friends.c

---
+++ friends.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Name
+    string name = get_string("What is your name? ");
+    
+    // Age
+    int age = get_int("What is your age? ")
+    
+    // Hometown
+    string hometown = get_string("What is your hometown? ");
+    
+    // Phone number
+    string number = get_string("What is your phone number? ");
+}
