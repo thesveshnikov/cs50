3b21cd778b54750aeaa15ad86804be98280af6e0
Sveshnikov <esbarros@proton.me>
2025-06-02 18:41:35 -0300

    Update buggy.c

--- buggy.c
+++ buggy.c
@@ -1,10 +1,18 @@
+#include <cs50.h>
 #include <stdio.h>
+
+void print_column(int height);
 
 int main(void)
 {
-    for (int = 0; i < 3; i++)
+    int h = get_int("Height");
+    print_column(h);
+}
+
+void print_column(int height)
+{
+    for (int i = 0; i < height; i++)
     {
-        printf("i is %i\n", i);
         printf("#\n");
     }
 }
