2f8ca2c3bb5a60ca9fb4bf08b1b6ee44cd364e21
Sveshnikov <esbarros@proton.me>
2025-06-03 17:23:47 -0300

    Update scores.c

--- scores.c
+++ scores.c
@@ -1,11 +1,13 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void);
 {
     int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
     
     // Print average
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
