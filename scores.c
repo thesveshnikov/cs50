ec2b3aa3ea5e0c634ec8f7d885dff56e441cc1f3
Sveshnikov <esbarros@proton.me>
2025-06-03 17:09:22 -0300

    Update scores.c

--- scores.c
+++ scores.c
@@ -2,10 +2,11 @@
 
 int main(void);
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
     
     // Print average
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
