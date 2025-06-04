9fdaae920407223b839413e77250f13d11003f7f
Sveshnikov <esbarros@proton.me>
2025-06-04 19:19:59 -0300

    Update string.c

--- string.c
+++ string.c
@@ -6,7 +6,7 @@ int main(void)
 {
     string s = get_string("Input:  ");
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
         printf("%c", s[i]);
     }
