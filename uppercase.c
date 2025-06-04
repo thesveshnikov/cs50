8d19a5c51c9068d7a55b94d15a322306b2ac91c5
Sveshnikov <esbarros@proton.me>
2025-06-04 19:40:26 -0300

    Update uppercase.c

--- uppercase.c
+++ uppercase.c
@@ -9,16 +9,7 @@ int main (void)
     printf("After");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            // Change s[i] to uppercase
-            printf("%c", toupper(s[i]));
-        }
-        else
-        {
-            // Just print s[i]
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
