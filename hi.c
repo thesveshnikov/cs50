7d759acf230f19992ee937bf1a3150d407d028cc
Sveshnikov <esbarros@proton.me>
2025-06-04 18:40:31 -0300

    Update hi.c

--- hi.c
+++ hi.c
@@ -7,6 +7,6 @@ int main(void)
     words[0] = "HI!";
     words[1] = "BYE!";
     
-    printf("%s\n", words[0]);
-    printf("%s\n", words[1]);
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
