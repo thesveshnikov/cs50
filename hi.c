d1834a3f5e1307d870a4a9a23125737f816441fe
Sveshnikov <esbarros@proton.me>
2025-06-04 18:34:31 -0300

    Update hi.c

--- hi.c
+++ hi.c
@@ -4,5 +4,8 @@
 int main(void)
 {
     string s = "HI!";
-    printf("%i %i %i %i %i %i\n", s[0], s[1], s[2], s[3] s[4] s[5]);
+    string t = "BYE!";
+    
+    printf("%s\n", s);
+    printf("%s\n", t);
 }
