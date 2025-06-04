3aa39e30b627137550a88077d046e3cb2ff6298b
Sveshnikov <esbarros@proton.me>
2025-06-04 20:21:23 -0300

    Update status.c

--- status.c
+++ status.c
@@ -6,7 +6,7 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Missing command-line argument\n");
-        return 1;
+        return 1132;
     }
     printf("hello, %s\n", argv[1]);
     return 0;
