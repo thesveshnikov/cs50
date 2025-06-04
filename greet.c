a7752bccf435a6d9a6a0dfd9e6d4b9213c536f4a
Sveshnikov <esbarros@proton.me>
2025-06-04 19:57:22 -0300

    Update greet.c

--- greet.c
+++ greet.c
@@ -1,8 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    string asnwer = get_string("What's your name? ");
-    printf("hello, %s\n", answer);
+    printf("hello, %s\n", argv[0]);
 }
