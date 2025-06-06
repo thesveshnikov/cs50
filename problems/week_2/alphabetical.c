03df0f28e49be17a61a20172ee628e367efb9b66
Sveshnikov <esbarros@proton.me>
2025-06-06 14:14:41 -0300

    Create alphabetical.c

---
+++ problems/week_2/alphabetical.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get user's input
+    string text = get_string("Input: \n");
+    
+    // Iterate through each element in the string
+        // check if it is alphabetical
+    
+    // Print out yes or no
+}
