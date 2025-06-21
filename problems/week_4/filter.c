f2b920b520f2a8f8592927c6e33d57a50387e6ab
Sveshnikov <esbarros@proton.me>
2025-06-21 14:38:16 -0300

    Create filter.c

---
+++ problems/week_4/filter.c
@@ -0,0 +1,13 @@
+#include "helpers.h"
+
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE gray = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = gray;
+        }
+    }
+}
